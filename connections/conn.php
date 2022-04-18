ec911b86e6ebbe3dce632b588a5657cc0767390a
Rouen Quirino <rouen2101@gmail.com>
2022-04-18 16:21:34 +0800

    add connection for database

---
+++ connections/conn.php
@@ -0,0 +1,28 @@
+<?php
+
+function connection(){
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "student_system";
+
+    //Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    //Check connection
+    if($conn->connect_error){
+        die("Connection failed: " . $conn->connect_error);
+    } else {
+        return $conn;
+    }
+}
+
+
+
+
+
+
+
+        
+    
+
+?>
