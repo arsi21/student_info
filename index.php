6cfefca9527d404844d388b1cc1420006c8f87ae
Rouen Quirino <rouen2101@gmail.com>
2022-04-18 16:22:42 +0800

    add html and php code for showing data in index.php

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php 
+
+include_once ("connections/conn.php");
+
+$conn = connection();
+
+$sql = "SELECT * FROM student_info ORDER BY id DESC";
+$result = $conn->query($sql);
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Information</title>
+</head>
+<body>
+    <h1>Student Information</h1>
+    <br>
+    <br>
+
+    <a href="add.php">Add New</a>
+
+    <table>
+        <thead>
+            <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+            </tr>
+        </thead>
+    
+        <tbody>
+    <?php //output data of each row
+    while($row = $result->fetch_assoc()) {?>
+            <tr>
+                <td><?php echo $row['first_name']; ?></td>
+                <td><?php echo $row['last_name']; ?></td>
+            </tr>
+    <?php }?>
+        </tbody>
+    </table>
+
+</body>
+</html>
