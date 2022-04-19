5ec150df9c1d43841f42ccb64ef43cc1cdd33c0c
Rouen Quirino <rouen2101@gmail.com>
2022-04-19 15:03:38 +0800

    add comments

--- server/add.php
+++ server/add.php
@@ -1,23 +1,29 @@
 <?php 
-
+//include server partial file
 include_once ("../server/conn.php");
 
+//start the connection
 $conn = connection();
 
+//check if the submit button is clicked
 if(isset($_POST['submit'])){
 
+    //get data from the form
     $fname = $_POST['fname'];
     $lname = $_POST['lname'];
     $gender = $_POST['gender'];
     $grade = $_POST['grade'];
     $bday = $_POST['bday'];
 
+    //query for inserting data into database
     $sql = "INSERT INTO `student_info`(`first_name`, `last_name`, 
     `gender`, `grade_level`, `birthday`) VALUES ('$fname', '$lname', '$gender', '$grade', '$bday')";
 
+    //insert data into database
     $conn->query($sql) or die ($conn->error);
 
-    echo header("Location: ../client/index.php");
+    //redirect to index.php
+    header("Location: ../client/index.php");
 }
 
 ?>
--- server/display.php
+++ server/display.php
@@ -2,8 +2,10 @@
 //include connection
 include_once ("../server/conn.php");
 
+//start connection
 $conn = connection();
 
+//for displaying all the names
 $sql = "SELECT * FROM student_info ORDER BY id DESC";
 $result = $conn->query($sql);
 
