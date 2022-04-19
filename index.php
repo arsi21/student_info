7b59affef3f4cf5d9ed3d8b18179e25187c4886c
Rouen Quirino <rouen2101@gmail.com>
2022-04-19 08:29:24 +0800

    add tooltip and add js for showing tooltip

--- index.php
+++ index.php
@@ -45,7 +45,7 @@ $result = $conn->query($sql);
     <div class="container">
         <h1>Student List</h1>
 
-        <a href="add-student.php" class="btn btn-primary btn-sm">Add New</a>
+        <a href="add-student.php" class="btn btn-primary btn-sm"  data-bs-toggle="tooltip" data-bs-placement="bottom" title="Add New Student">Add New</a>
     </div>
 
     <div class="container">
@@ -70,5 +70,11 @@ $result = $conn->query($sql);
     </div>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script>
+        var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+        var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+            return new bootstrap.Tooltip(tooltipTriggerEl)
+        })
+    </script>
 </body>
 </html>
