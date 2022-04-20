6095031cc387813b0d4a08016ecc51bbf4bac6a7
Rouen Quirino <rouen2101@gmail.com>
2022-04-20 21:40:47 +0800

    add style in login.php and fix some bugs

--- client/login.php
+++ client/login.php
@@ -7,17 +7,35 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Login</title>
 </head>
 <body>
-    <form action="" method="post">
-        <label for="username">Username</label>
-        <input type="text" id="username" name="username">
+    <div class="container">
+        <div class="row justify-content-center mt-5">
+            <div class="card col-md-7 col-lg-5">
+                <div class="card-body p-4">
+                    <h1 class="card-title mb-4">Login</h1>
 
-        <label for="password">Password</label>
-        <input type="password" id="password" name="password">
+                    <form action="" method="post">
+                        <div class="input-group mb-3">
+                            <span class="input-group-text" id="inputGroup-sizing-sm">Username</span>
+                            <input type="text" class="form-control" name="username">
+                        </div>
 
-        <button type="submit" name="login">Login</button>
-    </form>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text" id="inputGroup-sizing-sm">Password</span>
+                            <input type="password" class="form-control" name="password">
+                        </div>
+
+                        <?php if(isset($errorMsg)){ echo $errorMsg;}?>
+                        
+                        <button type="submit" class="btn btn-primary" name="login">Login</button>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
--- server/verify-login.php
+++ server/verify-login.php
@@ -16,20 +16,41 @@ if(isset($_POST['login'])){
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    $sql = "SELECT * FROM user WHERE username = '$username' AND password = '$password'";
-    $user = $con->query($sql) or die ($con->error);
-    $row = $user->fetch_assoc();
-    $total = $user->num_rows;
+    $sqlCheckUser = "SELECT * FROM user WHERE username = '$username'";
+    $user = $con->query($sqlCheckUser) or die ($con->error);
+    $rowUser = $user->fetch_assoc();
+    $totalUser = $user->num_rows;
 
-    //check if username and password match and existing
-    if($total > 0){
-        $_SESSION['username'] = $row['username'];
-        $_SESSION['access'] = $row['access'];
+    //check if username existing
+    if($totalUser > 0){
 
-        //redirect to index.php
-        header("Location: ../client/index.php");
+        $sqlCheckMatch = "SELECT * FROM user WHERE username = '$username' AND password = '$password'";
+        $match = $con->query($sqlCheckMatch) or die ($con->error);
+        $rowMatch = $match->fetch_assoc();
+        $totalMatch = $match->num_rows;
+
+        //check if username and password match
+        if($totalMatch > 0){
+            $_SESSION['username'] = $rowUser['username'];
+            $_SESSION['access'] = $rowUser['access'];
+
+            //redirect to index.php
+            header("Location: ../client/index.php");
+        }else{
+
+            $errorMsg = '
+                <div class="alert alert-warning" role="alert">
+                    Wrong password!
+                </div>
+            ';
+        }
+        
     }else{
-        echo "No username found!";
+        $errorMsg = '
+                <div class="alert alert-danger" role="alert">
+                    No username found!
+                </div>
+        ';
     }
 }
 
