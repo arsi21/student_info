a822f5de4e5154dab0a0999a771877d3a3f6c87c
Rouen Quirino <rouen2101@gmail.com>
2022-04-21 15:20:59 +0800

    add back button in login and signup page

--- client/login.php
+++ client/login.php
@@ -8,6 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
     <title>Login</title>
 </head>
 <body>
@@ -30,6 +31,9 @@
 
                         <?php if(isset($errorMsg)){ echo $errorMsg;}?>
                         
+                        <a class="btn btn-outline-dark me-3" href="index.php">
+                        <i class="bi bi-arrow-left-circle"></i> back</a>
+
                         <button type="submit" class="btn btn-primary" name="login">Login</button>
 
                     </form>
